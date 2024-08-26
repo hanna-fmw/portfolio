0838f280bc196ec1b4d3b54ff88e51356b4bbe72
Hanna Hosk <hanna.hosk@gmail.com>
2024-08-26 17:34:52 +0200

    Added skill set array to separate file

---
+++ app/data/skillsData.js
@@ -0,0 +1,13 @@
+export const skills = [
+	{ id: 1, name: 'HTML' },
+	{ id: 2, name: 'CSS, Tailwind, Shadcn' },
+	{ id: 3, name: 'JavaScript' },
+	{ id: 4, name: 'TypeScript' },
+	{ id: 5, name: 'React & Next.js (App Router)' },
+	{ id: 6, name: 'React Native' },
+	{ id: 7, name: 'Node.js & Express' },
+	{ id: 8, name: 'Prisma, Supabase & Firebase basics' },
+	{ id: 9, name: 'Git & GitHub' },
+	{ id: 10, name: 'Shopify & Squarespace' },
+	{ id: 11, name: 'UX/UI & animation basics (Figma, Framer Motion)' },
+]
