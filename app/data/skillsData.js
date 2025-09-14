5a1d1e28ad772c3fe1247fdca73e8b6415a318e0
Hanna Hosk <112755425+hanna-fmw@users.noreply.github.com>
2025-09-14 18:50:38 +0200

    Reorder and update skills list
    Moved 'Git & GitHub' to the end of the list and updated the 'Basic UX/UI & Animation' skill to include 'Figma, Framer Motion'.

--- app/data/skillsData.js
+++ app/data/skillsData.js
@@ -7,8 +7,8 @@ export const skills = [
 	{ id: 6, name: 'React, Next.js, React Native' },
 	{ id: 7, name: 'Node.js, Express.js' },
 	{ id: 8, name: 'Tailwind, CSS, Shadcn, etc.' },
-	{ id: 9, name: 'Git & GitHub' },
-	{ id: 10, name: 'Webflow CMS' },
-	{ id: 11, name: 'Shopify' },
-	{ id: 12, name: 'Basic UX/UI & Animation' },
+	{ id: 9, name: 'Webflow CMS' },
+	{ id: 10, name: 'Shopify' },
+	{ id: 11, name: 'Basic UX/UI & Animation (Figma, Framer Motion)' },
+	{ id: 12, name: 'Git & GitHub' },
 ]
